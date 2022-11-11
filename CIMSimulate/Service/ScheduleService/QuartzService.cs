3f6e694c45a208837d621d7f03b1d2963d6bb229
chester <ChesterWu@jquan.com.tw>
2022-11-11 10:58:37 +0800

    Add 新增套件

---
+++ CIMSimulate/Service/ScheduleService/QuartzService.cs
@@ -0,0 +1,23 @@
+﻿using Quartz;
+
+namespace CIMSimulate.Service.ScheduleService
+{
+    public class QuartzService
+    {
+        public QuartzService()
+        {
+
+        }
+        
+
+
+    }
+    public class Jobs : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
